22cb74898c7d249f94c4de236fe80cbffcfc142f
Kurt <ksphotopro@gmail.com>
2017-05-20 22:23:03 -0400

    fixed event listener returning game-piece parent bug

--- app.js
+++ app.js
@@ -2,4 +2,12 @@ gameBoard.gamePieces = gameBoard.loadGameBoardElements('game-piece');
 gameBoard.workingPairs = gameBoard.pickRandomPairs(gameBoard.gamePieces.length/2, allCodes);
 gameBoard.matchPairs = gameBoard.createAssignments(gameBoard.workingPairs);
 gameBoard.createIconTiles(gameBoard.gamePieces, gameBoard.matchPairs);
-console.log(gameBoard.matchPairs);
+
+const liveBoard = document.getElementById('game-board');
+console.log(liveBoard);
+liveBoard.addEventListener('click', function(e){
+    if (e.target != liveBoard){
+        console.log(e.target);
+    }
+})
+//console.log(gameBoard.matchPairs);
